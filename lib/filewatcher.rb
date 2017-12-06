f5d7f797063d03e70093516d53c64712e0ceab5f
Mathias Slawik <mathias.slawik@tu-berlin.de>
2017-12-06 11:38:12 +0100

    Only trap supported signals (#86)
    * Only trap supported signals (fix exception in Ruby on Windows)
    
    * Addressed comments by @AlexWayfer

--- lib/filewatcher.rb
+++ lib/filewatcher.rb
@@ -29,7 +29,9 @@ class Filewatcher
   end
 
   def watch(&on_update)
-    %w[HUP INT TERM].each { |signal| trap(signal) { exit } }
+    ## The set of available signals depends on the OS
+    ## Windows doesn't support `HUP` signal, for example
+    (%w[HUP INT TERM] & Signal.list.keys).each { |signal| trap(signal) { exit } }
     @on_update = on_update
     @keep_watching = true
     yield('', '') if @immediate
