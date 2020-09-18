7451d64ec5eeb823dbcb45ad1b45f9ebf3d02cc2
Alexander Popov <alex.wayfer@gmail.com>
2020-09-19 02:45:23 +0300

    Make `VERSION` specs more accurate

--- spec/filewatcher/version_spec.rb
+++ spec/filewatcher/version_spec.rb
@@ -5,7 +5,5 @@ require_relative '../../lib/filewatcher/version'
 describe 'Filewatcher::VERSION' do
   subject { Object.const_get(self.class.description) }
 
-  it { is_expected.to be_kind_of String }
-
-  it { is_expected.not_to be_empty }
+  it { is_expected.to match(/^\d+\.\d+\.\d+(\.\w+(\.\d+)?)?$/) }
 end
