e225741747b26b4bd55c017676d60e06b02d987d
Alexander Popov <alex.wayfer@gmail.com>
2022-10-01 20:55:38 +0300

    Replace `codecov` Ruby uploader with shell one

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start
 
-if ENV['CODECOV_TOKEN']
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['CI']
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
+
+SimpleCov.start
 
 require_relative '../lib/filewatcher/spec_helper'
 
